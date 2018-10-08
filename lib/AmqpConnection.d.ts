d73ff242852d525b99f8ada1dce8a9390a4a24fe
Uldis Jansons <uldisj@intranet.smc.lv>
2018-10-08 14:03:22 +0300

    Delete transpilde files

--- lib/AmqpConnection.d.ts
+++
@@ -1,8 +0,0 @@
-import AmqpChannelQueue from './AmqpChannelQueue';
-export default class AmqpConnection {
-    private readonly _endpoint;
-    private _connection;
-    constructor(endpoint: string);
-    connect(): Promise<void>;
-    createQueueChannel(queueName: String): AmqpChannelQueue;
-}
--- lib/CorrelationIdFactory.d.ts
+++
@@ -1,3 +0,0 @@
-export default class CorrelationIdFactory {
-    create(): String;
-}
