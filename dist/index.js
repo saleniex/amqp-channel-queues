664bbbec3dd2ac7b7efad89aed1007faab9f0a27
Uldis Jansons <uldisj@intranet.smc.lv>
2018-10-08 16:07:37 +0300

    Add index transpiled files to repo

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const tslib_1 = require("tslib");
+tslib_1.__exportStar(require("./AmqpConnection"), exports);
+tslib_1.__exportStar(require("./AmqpChannelQueue"), exports);
+tslib_1.__exportStar(require("./CorrelationIdFactory"), exports);
+//# sourceMappingURL=index.js.map
