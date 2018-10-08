e6bdb8d252f11bdeefe23ca4e8b584c0e49cd259
Uldis Jansons <uldisj@intranet.smc.lv>
2018-10-08 11:26:26 +0300

    Add dist folder to repo

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+export * from './src/AmqpConnection';
+export * from './src/AmqpChannelQueue';
+export * from './src/CorrelationIdFactory';
--- index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports.AmqpChannelQueue = require('./dist/AmqpChannelQueue');
-module.exports.AmqpConnection = require('./dist/AmqpConnection');
-module.exports.CorrelationIdFactory = require('./dist/CorrelationIdFactory');
-
-
